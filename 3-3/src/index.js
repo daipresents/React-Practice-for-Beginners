96581a034e1aa99e82b0b2387bfd13359fd24c02
Dai Fujihara <daipresents@gmail.com>
2024-05-29 11:34:23 +0900

    Fixed 3-3-4

--- 3-3/src/index.js
+++ 3-3/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ListTemplate from './ListTemplate';
+import RenderProps from './RenderProps';
 import StyledPanel from './StyledPannel';
 import TitledPanel from './TitledPanel';
+import books from './books';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +15,32 @@ const body = <p>ようこそ、WINGSプロジェクトへ！</p>;
 root.render(
   <React.StrictMode>
     <>
+      <RenderProps src={books} render={renderElem => (
+        <>
+          <dt>
+            <a href={`https://wings.msn.to/books/${renderElem.isbn}/${renderElem.isbn}.jpg`}>
+              {renderElem.title} ({renderElem.price}円)
+            </a>
+          </dt>
+          <dd>{renderElem.summary}</dd>
+        </>
+      )}>
+      </RenderProps>
+      <hr />
+      <ListTemplate src={books}>
+        {book => (
+          <>
+            <dt>
+              <a href={`https://wings.msn.to/books/${book.isbn}/${book.isbn}.jpg`}>
+                {book.title} ({book.price}円)
+              </a>
+            </dt>
+            <dd>{book.summary}</dd>
+
+          </>
+        )}
+      </ListTemplate>
+      <hr />
       <StyledPanel>
         <p>メンバー募集中！</p>
         <p>ようこそ、WINGSプロジェクトへ！</p>
