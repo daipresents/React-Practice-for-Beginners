9fabdfda83342d9e1740c28d235826661ce453a4
Dai Fujihara <daipresents@gmail.com>
2024-05-29 16:20:49 +0900

    Fixed 4-1-1

---
+++ 4-1/src/StateFrom.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+// 制御コンポーネント
+export default function StateForm() {
+  const [form, setForm] = useState({
+    name: '山田太郎',
+    age: 18
+  });
+
+  const handleForm = e => {
+    setForm({
+      ...form,
+      //算出プロパティ名
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const show = () => {
+    console.log(`こんにちは、${form.name} (${form.age}) 歳さん！`);
+  };
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="name">名前：</label>
+        <input id="name" name="name" type="text" onChange={handleForm} value={form.name} />
+      </div>
+      <div>
+        <label htmlFor="age">年齢：</label>
+        <input id="age" name="age" type="number" onChange={handleForm} value={form.age} />
+      </div>
+      <div>
+        <button type="button" onClick={show}>
+          送信
+        </button>
+      </div>
+      <p>こんにちは、 {form.name} ({form.age} 歳）さん！</p>
+    </form>
+  );
+}
