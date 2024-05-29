313cf215764ff3788504d2e69b0c7941748273d6
Dai Fujihara <daipresents@gmail.com>
2024-05-29 11:18:32 +0900

    Fixed 3-3-2

--- 3-3/src/index.js
+++ 3-3/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import StyledPanel from './StyledPannel';
+import TitledPanel from './TitledPanel';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const title = <p>メンバー募集中！</p>;
+const body = <p>ようこそ、WINGSプロジェクトへ！</p>;
+
 root.render(
   <React.StrictMode>
     <>
@@ -12,11 +16,9 @@ root.render(
         <p>メンバー募集中！</p>
         <p>ようこそ、WINGSプロジェクトへ！</p>
       </StyledPanel>
+      <hr />
+      <TitledPanel title={<p>メンバー募集中！</p>} body={<p>ようこそ、WINGSプロジェクトへ！</p>} />
+      <TitledPanel title={title} body={body} />
     </>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
