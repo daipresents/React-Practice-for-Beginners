f937af6f06969f59110d607c13ee83b09b3b65be
Dai Fujihara @daipresents <daipresents@gmail.com>
2024-05-30 09:54:23 +0900

    Fixed 4-1-3

--- 4-1/src/index.js
+++ 4-1/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import StateForm from './StateFrom';
+import StateFormUC from './StateFromUC';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,6 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
+      <StateFormUC />
+      <hr />
+
       <StateForm />
     </>
   </React.StrictMode>
