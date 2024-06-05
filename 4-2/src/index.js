c6fb48c3a503784f69b1c0d48048c8d36b48f39a
Dai Fujihara <daipresents@gmail.com>
2024-06-05 10:30:42 +0900

    Fixed 4-2-2

--- 4-2/src/index.js
+++ 4-2/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StateNest from './StateNext';
+import StateNest from './StateNest';
+import StateNestImmer from './StateNestImmer';
+import StateNestImmer2 from './StateNestImmer2';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,6 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
+      <StateNestImmer2 />
+      <hr />
+      <StateNestImmer />
+      <hr />
       <StateNest />
     </>
   </React.StrictMode>
