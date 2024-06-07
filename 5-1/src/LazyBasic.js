dccb536a9d1927aa99c6d27e14f9449b98636841
Dai Fujihara <daipresents@gmail.com>
2024-06-07 14:48:28 +0900

    Fixed 5-1-1

---
+++ 5-1/src/LazyBasic.js
@@ -0,0 +1,24 @@
+import { Suspense, lazy } from 'react';
+import MyLoading from './MyLoading';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const LazyButton = lazy(() => sleep(2000).then(() => import('./LazyButton')));
+const LazyButton2 = lazy(() => sleep(1000).then(() => import('./LazyButton2')));
+
+export default function LazyBasic() {
+  return (
+    <>
+      <Suspense fallback={<MyLoading />}>
+        <LazyButton />
+        <LazyButton2 />
+      </Suspense>
+
+      <Suspense fallback={<MyLoading />}>
+        <LazyButton />
+      </Suspense>
+
+      <div>Lazyじゃない</div>
+      <LazyButton2 />
+    </>
+  );
+}
