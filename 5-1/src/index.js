7ede2bdaf51971492ceb22b8f935b34325ffe0f3
Dai Fujihara <daipresents@gmail.com>
2024-06-07 15:58:16 +0900

    Fixed 5-1-2

--- 5-1/src/index.js
+++ 5-1/src/index.js
@@ -2,21 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LazyBasic from './LazyBasic';
 import LazyMulti from './LazyMulti';
+import SuspenseResult from './SuspenseResult';
+import SuspenseSimple from './SuspenseSimple';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
+      <SuspenseResult />
+      <hr />
+      <SuspenseSimple />
+      <hr />
       <LazyMulti />
       <hr />
       <LazyBasic />
     </>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
