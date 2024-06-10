d4d6a3d27a7c4450025f028127729dea642b0b0c
Dai Fujihara <daipresents@gmail.com>
2024-06-10 17:13:03 +0900

    Fixed 5-3-1

--- 5-3/src/index.js
+++ 5-3/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import PortalBasic from './PortalBasic';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <div id="dialog"></div>
+      <PortalBasic />
+    </>
   </React.StrictMode>
 );
 
