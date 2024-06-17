08212c22cd525762cd045f695c3367220f0fefd6
Dai Fujihara <daipresents@gmail.com>
2024-06-17 16:35:27 +0900

    useEffectの説明を追記。

--- 7-1/src/StateEffect.js
+++ 7-1/src/StateEffect.js
@@ -4,23 +4,27 @@ export default function StateEffect({ init }) {
   const [count, setCount] = useState(init);
   const [hoge, setHoge] = useState('hoge');
 
-  // フックは関数コンポーネントのトップレベルで呼び出すこと
+  /**
+   * フックは関数コンポーネントのトップレベルで呼び出すこと
+   * useEffect関数は副作用フックとも呼ばれる。
+   * コンポーネントの状態（PropsやState）が変化したタイミングで実行すべき処理を定義できる。
+   * 特別な理由がなければ、引数depsは常に明示すべき。
+   * depsがないとuseEffect関数の処理は再描画時に常に実行される
+        useEffect(() => {
+          console.log(`count is ${count}`);
+        });
+   *
+   * depsが [] だと実行のトリガーになる変数がないので、コンポーネントの初期描画時にのみ処理を実行する
+   * 再描画時に処理が実行されることはない
+        useEffect(() => {
+          console.log(`count is ${count}`);
+        }, []);
+   */
+
   useEffect(() => {
     console.log(`count is ${count}`);
   }, [count]);
 
-  // 特別な理由がなければ、引数depsは常に明示すべき。
-  //
-  // depsがないとuseEffect関数の処理は再描画時に常に実行される
-  // useEffect(() => {
-  //   console.log(`count is ${count}`);
-  // });
-  //
-  // depsが [] だと実行のトリガーになる変数がないので、コンポーネントの初期描画時にのみ処理を実行する
-  // 再描画時に処理が実行されることはない
-  // useEffect(() => {
-  //   console.log(`count is ${count}`);
-  // }, []);
   const handleClick = () => setCount(count + 1);
 
   return (
