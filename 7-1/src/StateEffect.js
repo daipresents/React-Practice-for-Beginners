8564d6488d086cb8f837ea90cfc38960437d0e12
Dai Fujihara <daipresents@gmail.com>
2024-06-17 15:07:00 +0900

    Fixed 7-1-2

--- 7-1/src/StateEffect.js
+++ 7-1/src/StateEffect.js
@@ -9,6 +9,18 @@ export default function StateEffect({ init }) {
     console.log(`count is ${count}`);
   }, [count]);
 
+  // 特別な理由がなければ、引数depsは常に明示すべき。
+  //
+  // depsがないとuseEffect関数の処理は再描画時に常に実行される
+  // useEffect(() => {
+  //   console.log(`count is ${count}`);
+  // });
+  //
+  // depsが [] だと実行のトリガーになる変数がないので、コンポーネントの初期描画時にのみ処理を実行する
+  // 再描画時に処理が実行されることはない
+  // useEffect(() => {
+  //   console.log(`count is ${count}`);
+  // }, []);
   const handleClick = () => setCount(count + 1);
 
   return (
