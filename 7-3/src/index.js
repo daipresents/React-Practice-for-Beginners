965c75076798f80aa0a332047eb2fb4b2514611b
Dai Fujihara <daipresents@gmail.com>
2024-06-18 09:25:05 +0900

    Fixed 7-3-4

--- 7-3/src/index.js
+++ 7-3/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import HookReducer from './HookReducer';
+import HookreducerUp from './HookReducerUp';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>Reducerを複数のAction型に対応する</h1>
+    <HookreducerUp init={0} />
+
     <h1>useReducer フックの基本</h1>
     <HookReducer init={0} />
   </React.StrictMode>
