15c557aae3dc73a3e686ddf26b7d1b270eaf3f74
Dai Fujihara <daipresents@gmail.com>
2024-06-21 10:33:04 +0900

    Fixed 7-6-3

--- 7-6/src/HookMemoChild.js
+++ 7-6/src/HookMemoChild.js
@@ -1,15 +1,18 @@
-export const MyButton = ({ id, handleClick, children }) => {
+import { memo } from 'react';
+
+// memo関数でコンソールログが4回 ＞ 3回 になる。
+export const MyButton = memo(({ id, handleClick, children }) => {
   console.log(`MyButton is called: ${id}`);
 
   return (
     <button onClick={handleClick}>{children}</button>
   );
-};
+});
 
-export const MyCounter = ({ id, value }) => {
+export const MyCounter = memo(({ id, value }) => {
   console.log(`MyCounter is called: ${id}`);
 
   return (
     <p>現在値： {value}</p>
   );
-};
+});
