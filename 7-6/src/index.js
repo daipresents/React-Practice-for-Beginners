fa9b429e30acc697fc67238a323f4e84149834aa
Dai Fujihara <daipresents@gmail.com>
2024-06-21 10:23:51 +0900

    Fixed 7-6-1

--- 7-6/src/index.js
+++ 7-6/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HookMemo from './HookMemo';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <HookMemo />
+    </>
   </React.StrictMode>
 );
 
