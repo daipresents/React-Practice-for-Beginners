9ac344a87f2451e8be7131e06b3eca595268989f
Dai Fujihara <daipresents@gmail.com>
2024-06-21 14:59:33 +0900

    Fixed 7-7-2

--- 7-7/src/HookTransition.js
+++ 7-7/src/HookTransition.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import { BookDetails, CommentList } from './HookTransitionChild';
 import books from './books';
 import commentList from './comments';
@@ -7,10 +7,17 @@ export default function HookTransition() {
   const [isbn, setIsbn] = useState('');
   const [comments, setComments] = useState([]);
 
+  // トランジションを利用するための準備
+  const [isPending, startTransition] = useTransition();
+
   const handleChange = e => {
     const isbn = e.target.value;
     setIsbn(isbn);
-    setComments(commentList.filter(c => c.isbn === isbn));
+    // トランジションのは以下でStateを管理する
+    // これでCommentsの更新が優先度低くなる
+    startTransition(() => {
+      setComments(commentList.filter(c => c.isbn === isbn));
+    });
   };
 
   return (
