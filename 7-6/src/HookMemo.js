a0efa5c817fd67d1382037c129a3b9f12f184547
Dai Fujihara <daipresents@gmail.com>
2024-06-21 10:37:13 +0900

    Fixed 7-6-4

--- 7-6/src/HookMemo.js
+++ 7-6/src/HookMemo.js
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { MyButton, MyCounter } from './HookMemoChild';
 
 const sleep = delay => {
@@ -10,9 +10,11 @@ export default function HookMemo() {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  const increment = () => setCount1(c => c + 1);
-  const decrement = () => setCount2(c => c - 1);
+  // 関数そのものをMemo。これでログが1行だけ表示される。
+  const increment = useCallback(() => setCount1(c => c + 1), []);
+  const decrement = useCallback(() => setCount2(c => c - 1), []);
 
+  // count1だけ反応するので、 count2は影響を受けない。
   const heavyProcess = useMemo(() => {
     sleep(1000);
     return count1 + 100;
