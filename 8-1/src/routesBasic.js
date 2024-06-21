53c0416c8f2ffd718c6d24627129dc1ca8479e6c
Dai Fujihara @daipresents <daipresents@gmail.com>
2024-06-21 22:26:53 +0900

    Fixed 8-1-3

--- 8-1/src/routesBasic.js
+++ 8-1/src/routesBasic.js
@@ -1,12 +1,30 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import AboutPage from './AboutPage';
 import ArticlePage from './ArticlePage';
 import TopPage from './TopPage';
 
-const routesBasic = createBrowserRouter([
-  { path: '/', element: <TopPage /> },
-  { path: '/article', element: <ArticlePage /> },
-  { path: '/about', element: <AboutPage /> },
-]);
+//const routesBasic = createBrowserRouter(
+// オブジェクト配列で定義
+// [
+//   { path: '/', element: <TopPage /> },
+//   { path: '/article', element: <ArticlePage /> },
+//   { path: '/about', element: <AboutPage /> },
+// ],
+// basename指定も可能
+// {
+//   basename: '/myapp',
+// }
+//);
+
+// タグ形式でも記載できる。
+const routesBasic = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<TopPage />} />
+      <Route path="/article" element={<ArticlePage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </>
+  )
+);
 
 export default routesBasic;
