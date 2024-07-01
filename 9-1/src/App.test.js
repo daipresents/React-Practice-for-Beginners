f61ce72c518dd37cd80060989d4542eb48512a2f
Dai Fujihara <daipresents@gmail.com>
2024-07-01 17:07:21 +0900

    Fixed 9-1-2

--- 9-1/src/App.test.js
+++ 9-1/src/App.test.js
@@ -2,7 +2,27 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // 描画結果をコンソール上で確認する場合
+  //const { debug, baseElement } = render(<App />);
+  //debug(baseElement);
+
+  // containerオプションで文書ツリーを置き換える
+  // const para = document.createElement('p');
+  // const { debug, baseElement } = render(<App />, {
+  //   container: document.body.appendChild(para)
+  // });
+  // debug(baseElement);
+  // 以下のようにPが入る。
+  // <p>
+  // <div
+  //   class="App"
+  // >
+  //   <header
+  //     class="App-header"
+  //   >
+
   render(<App />);
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
